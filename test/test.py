956aaf875c91ab30924837971b040b38aecfd2ac
Juan de Dios <juan.dios.flores@gmail.com>
2017-06-06 10:18:42 +0200

    almost done

--- test/test.py
+++ test/test.py
@@ -23,8 +23,8 @@ class Test_solver(unittest.TestCase):
         ro = 6
         bet = 8/3
         param = [sig, ro, bet]
-        t_d = 0.0001
-        N = 80000
+        t_d = 0.001
+        N = 50000
         states = lorenz.run.run_lorenz(param, state, t_d, N)
         w_states = lorenz.util.wikipedia_lorenz(param, state, t_d, N)
         #self.assertAlmostEqual(states.tolist(), w_states.tolist())
@@ -34,13 +34,13 @@ class Test_solver(unittest.TestCase):
         """
         The test for my solver 2
         """
-        state = [1.0, 1.0, 1.0]
+        state = [1.0, -5.0, 13.0]
         sig = 10
         ro = 16
         bet = 8/3
         param = [sig, ro, bet]
-        t_d = 0.0001
-        N = 80000
+        t_d = 0.001
+        N = 50000
         states = lorenz.run.run_lorenz(param, state, t_d, N)
         w_states = lorenz.util.wikipedia_lorenz(param, state, t_d, N)
         #self.assertAlmostEqual(states.tolist(), w_states.tolist())
@@ -50,13 +50,47 @@ class Test_solver(unittest.TestCase):
         """
         The test for my solver 3
         """
-        state = [1.0, 1.0, 1.0]
+        state = [1.0, -5.0, 13.0]
         sig = 10
         ro = 28
         bet = 8/3
         param = [sig, ro, bet]
         t_d = 0.0001
-        N = 80000
+        N = 50000
+        states = lorenz.run.run_lorenz(param, state, t_d, N)
+        w_states = lorenz.util.wikipedia_lorenz(param, state, t_d, N)
+        #self.assertAlmostEqual(states.tolist(), w_states.tolist())
+        self.assertTrue(np.allclose(states, w_states, 
+                                    rtol = 2e-1, atol = 1))
+
+    def test_solver4(self):
+        """
+        The test for my solver 4
+        """
+        state = [1.0, -5.0, 13.0]
+        sig = 14
+        ro = 28
+        bet = 8/3
+        param = [sig, ro, bet]
+        t_d = 0.0001
+        N = 50000
+        states = lorenz.run.run_lorenz(param, state, t_d, N)
+        w_states = lorenz.util.wikipedia_lorenz(param, state, t_d, N)
+        #self.assertAlmostEqual(states.tolist(), w_states.tolist())
+        self.assertTrue(np.allclose(states, w_states, 
+                                    rtol = 2e-1, atol = 1))
+
+    def test_solver5(self):
+        """
+        The test for my solver 5
+        """
+        state = [1.0, -5.0, 13.0]
+        sig = 14
+        ro = 28
+        bet = 13/3
+        param = [sig, ro, bet]
+        t_d = 0.0001
+        N = 50000
         states = lorenz.run.run_lorenz(param, state, t_d, N)
         w_states = lorenz.util.wikipedia_lorenz(param, state, t_d, N)
         #self.assertAlmostEqual(states.tolist(), w_states.tolist())
